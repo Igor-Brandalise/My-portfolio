609b516977dfc51cd77bfd5204bf40b6681b9fcb
Igor Brandalise <brandaliseigor@gmail.com>
2025-01-01 23:10:22 -0300

    add particles.js

---
+++ particles.js
@@ -0,0 +1,50 @@
+const options = {
+    background: {
+      color: "#000", 
+    },
+    interactivity: {
+      events: {
+        onClick: {
+          
+          enable: true,
+          mode: "push", 
+        },
+        onHover: {
+          
+          enable: true,
+          mode: "repulse",
+        },
+      },
+      modes: {
+        push: {
+          quantity: 6, 
+        },
+        repulse: {
+          distance: 100,
+        },
+      },
+    },
+    particles: {
+      links: {
+        enable: true, 
+        opacity: 0.3,
+        distance: 200,
+        
+      },
+      move: {
+        enable: true, 
+        speed: { min: 1, max: 3 },
+      },
+      opacity: {
+        value: { min: 0.3, max: 0.7 }, 
+      },
+      size: {
+        value: { min: 1, max: 3 }, 
+      },
+
+      
+      
+    },
+  };
+
+  tsParticles.load("tsparticles", options);
