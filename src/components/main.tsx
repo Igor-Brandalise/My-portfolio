d49980230290271f161b8b2580dd4fb6c1178430
Igor-Brandalise <brandaliseigor123@gmail.com>
2026-03-26 14:54:39 -0300

    refactor: refazendo o layout da home

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,85 +1,69 @@
+import { Download, MapPin } from "lucide-react";
+
 export function Main() {
   return (
-    <main className="flex flex-col md:flex-row text-amber-50 z-50 items-center min-h-[115vh] md:min-h-[70vh] ">
-      <section className="flex flex-col pl-[1.8rem] pt-20 md:pt-0 lg:ml-15 xl:ml-28 lg:w-200 z-40">
-        <h1 className="z-40 text-[2.5rem] font-bold md:text-[3rem] lg:text-[3.5rem] xl:text-[4.2rem] leading-12 md:leading-20">
-          Hi There, <br /> I'm Igor{" "}
-          <span className="text-red-600">Brandalise</span>
+    <main className=" reveal relative mx-auto w-[85%] md:w-[70%]  flex flex-col justify-center md:flex-row text-[#e5e5e5] items-center  pt-20 pb-30 lg:pt-35  gap-12 ">
+      <section className="flex flex-col flex-1 mt-8 z-40">
+        <span
+          className="mt-6 mb-6 h-9 rounded-[30px] inline-flex items-center justify-center w-60 bg-red-950/80 border border-red-500/50 text-[#ff3b3b]
+                  text-sm font-medium tracking-wide 
+                 shadow-[0_0_15px_rgba(239,68,68,0.1)]"
+        >
+          🚀 Welcome to my portfolio
+        </span>
+
+        <h1 className=" typing-title text-3xl font-bold text-[#FAFAF9] sm:text-4xl z-10">
+          Front-end <span className="text-[#ff3b3b]">developer</span>
         </h1>
 
-        <h2 className="pt-4 font-bold text-[1.4rem] text-zinc-50 z-50">
-          Front-end developer
-        </h2>
-
-        <p className="pt-4 mr-4 text-zinc-200 z-50 indent-1.5 max-w-2xl leading-4.5 md:leading-6">
-          Olá, meu nome é Igor Brandalise, sou apaixonado no mundo da tecnologia
-          e na área de programação. Comecei a estudar programação web em 2023 e
-          me sinto cada vez mais inspirado a seguir minha carreira na área. Meu
-          objetivo atual é conquistar meu primeiro emprego na área e futuramente
-          me tornar um Tech Lead.
+        <p className="mt-6 max-w-2xl text-lg text-[#A8A29E] leading-relaxed tracking-wide md:max-w-[30em]">
+          Hi, I'm Igor Brandalise. Developer since 2023, focused on TypeScript,
+          React, Next.js, and Node.js. I'm currently building modern projects to
+          land my first role in tech, with the long-term goal of evolving into a
+          Tech Lead. Dedicated to continuous learning and clean code.
         </p>
 
         <a
           href="/document/Igor Brandalise - CV.pdf"
           download
-          className="z-50 w-28 h-10 mt-8 "
+          className="z-50 mt-8 block md:w-fit"
         >
-          <button className="bg-amber-50 font-bold rounded-lg text-black w-28 h-10 z-50 transition-all duration-200 hover:bg-neutral-400 hover:ring-2 hover:ring-neutral-900 hover:ring-offset-2 ">
-            Download CV
+          <button
+            className="group relative px-8 h-14 inline-flex items-center justify-center gap-3 
+               bg-[#ff3b3b] text-black font-extrabold rounded-[15px] z-50
+               animate-bounce hover:animate-none transition-all duration-300
+               shadow-[0_0_15px_rgba(255,59,59,0.3)] 
+               hover:shadow-[0_0_35px_rgba(255,59,59,0.7)] w-[95%] md:w-auto"
+          >
+            <Download
+              size={22}
+              className="transition-transform group-hover:scale-110"
+            />
+            <span className="uppercase tracking-wider text-sm">
+              Download CV
+            </span>
+
+            <div className="absolute inset-0 rounded-[15px] bg-linear-to-b from-white/20 to-transparent pointer-events-none"></div>
           </button>
         </a>
       </section>
 
-      <section className=" h-auto p-7    md:mt-12 z-50 ">
+      <section className="hidden md:hidden  lg:flex relative w-fit">
         <img
-          src="/igor.jpeg"
+          src="/pf.jpeg"
           alt="Igor Brandalise"
-          className="md:h-[300px] md:w-[300px] rounded-[50%] w-[200px] h-[200px]  "
+          className="h-[300px] w-[300px] object-cover rounded-[10px] "
         />
-      </section>
 
-      <section className="transform scale-0 hidden xl:scale-100 lg:bg-(--bg-pers) lg:border border-(--b-color) lg:rounded-[60px] lg:p-2 lg:items-center lg:h-100 lg:flex lg:flex-col lg:w-24 lg:m-4 lg:mt-12 lg:pt-10 lg:ml-30 lg:gap-6 w-40 ">
-        <a href="https://github.com/Igor-Brandalise" target="_blank">
-          <img
-            src="/icons8-github-48.png"
-            alt="github logo"
-            height="60px"
-            width="60px"
-          />
-        </a>
-
-        <a href="https://instagram.com/igor_brandalise" target="_blank">
-          <img
-            src="/icons8-instagram-50.png"
-            alt="instagram logo"
-            height="60px"
-            width="60px"
-          />
-        </a>
-
-        <a
-          href="https://www.linkedin.com/in/igor-brandalise-a08280302/"
-          target="_blank"
+        <div
+          className="flex items-center absolute bottom-0 left-0 -translate-x-1/2 translate-y-1/2 
+                bg-[#ff3b3b] text-black p-2 z-10 text-[14px] font-bold rounded-sm gap-2
+                shadow-[0_4px_10px_rgba(255,59,59,0.3)] 
+                hover:scale-105 transition-transform duration-300 cursor-default"
         >
-          <img
-            src="/icons8-linkedin-50.png"
-            alt="linkedin logo"
-            height="60px"
-            width="60px"
-          />
-        </a>
-
-        <a
-          href="https://mail.google.com/mail/u/0/?fs=1&tf=cm&source=mailto&to=brandaliseigor123@gmail.com"
-          target="_blank"
-        >
-          <img
-            src="/icons8-gmail-64.png"
-            alt="gmail logo"
-            height="60px"
-            width="60px"
-          />
-        </a>
+          <MapPin size={15} />
+          SC, Brazil
+        </div>
       </section>
     </main>
   );
