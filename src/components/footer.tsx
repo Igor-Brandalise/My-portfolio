6b93c1aecea521adfee37326d051b0ad56f2f561
Igor-Brandalise <brandaliseigor123@gmail.com>
2026-03-26 14:48:28 -0300

    refactor(ui): redesenho completo do footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,18 +1,20 @@
 export function Footer() {
   return (
-    <footer className="text-white blur-custom z-50 w-full bottom-0 flex justify-between p-4 border-t-custom h-16 fixed items-center">
-      <span className="md:ml-4">&copy; Igor Brandalise</span>
-      <span className="md:mr-4">
-        Icons by{" "}
-        <a
-          href="https://icons8.com"
-          target="_blank"
-          className="text-amber-50 hover:text-red-600"
-        >
-          {" "}
-          Icons8
-        </a>
-      </span>
+    <footer className="reveal mt-auto text-white bg-[#121212]/95  z-50 w-full bottom-0 flex justify-between  border-t-custom h-18 absolute items-center">
+      <div className="w-[85%] md:w-[70%] flex justify-between relative mx-auto">
+        <span>&copy; Igor Brandalise</span>
+        <span>
+          Icons by{" "}
+          <a
+            href="https://icons8.com"
+            target="_blank"
+            className="text-amber-50 hover:text-[#ff3b3b] "
+          >
+            {" "}
+            Icons8
+          </a>
+        </span>
+      </div>
     </footer>
   );
 }
