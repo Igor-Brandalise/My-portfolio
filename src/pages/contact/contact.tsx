211f343c0c13d0766d4e2a38e76893867bbc8b1e
Igor-Brandalise <brandaliseigor123@gmail.com>
2026-03-26 14:49:32 -0300

    style: indentação do componente

--- src/pages/contact/contact.tsx
+++ src/pages/contact/contact.tsx
@@ -2,12 +2,12 @@ import { Contacts } from "../../components/contact";
 import { Footer } from "../../components/footer";
 import { Header } from "../../components/header";
 
-export function Contact(){
-    return (
-        <div>
-            <Header />
-            <Contacts />
-            <Footer />
-        </div>
-    )
+export function Contact() {
+  return (
+    <div>
+      <Header />
+      <Contacts />
+      <Footer />
+    </div>
+  );
 }
